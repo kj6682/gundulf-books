5db64cb94942cca2b644cf55002ea1c9d93607a1
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2018-10-14 12:49:33 +0200

    zero commit

---
+++ src/main/java/org/kj6682/gundulfbooks/GundulfBooksApplication.java
@@ -0,0 +1,12 @@
+package org.kj6682.gundulfbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundulfBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundulfBooksApplication.class, args);
+	}
+}
