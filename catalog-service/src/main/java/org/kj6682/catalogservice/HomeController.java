d91888902deb9d03a1a3eca92933ccd32130ab64
KJ6682 <luigi.mastroserio@gmail.com>
2023-05-31 21:21:02 +0200

    [IMP] first commit
    not so much to show here

---
+++ catalog-service/src/main/java/org/kj6682/catalogservice/HomeController.java
@@ -0,0 +1,13 @@
+package org.kj6682.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")                
+    public String getGreeting() {
+      return "Welcome to the book catalog!";
+    }
+}//:)
