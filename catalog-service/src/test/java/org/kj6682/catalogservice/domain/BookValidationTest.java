ad275408d9847d724a0ae589eddf3a67b96f8831
KJ6682 <luigi.mastroserio@gmail.com>
2023-06-06 16:00:16 +0200

    [test] unit test pass

--- catalog-service/src/test/java/org/kj6682/catalogservice/domain/BookValidationTest.java
+++ catalog-service/src/test/java/org/kj6682/catalogservice/domain/BookValidationTest.java
@@ -1,6 +1,8 @@
 package org.kj6682.catalogservice.domain;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -9,9 +11,9 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-
 
 public class BookValidationTest {
     private static Validator validator;
@@ -23,43 +25,87 @@ public class BookValidationTest {
     }
 
     @Test
-    void given_all_fields_are_correct__then_validation_succeeds() {
+    void when_all_fields_are_correct__then_validation_succeeds() {
         var book = new Book("0123456789", "George Orwell", "1984", 10.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).isEmpty();
     }
 
     @Test
-    void given_isbn_is_missing__then_validation_fails(){
-        fail("test not implemented yet");
+    void when_isbn_is_missing__then_validation_fails() {
+        var book = new Book("", "George Orwell", "1984", 10.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(2);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book ISBN must be defined.")
+                .contains("The ISBN format must be valid.");
     }
 
     @Test
-    void given_isbn_is_incorrect__then_validation_fails(){
-        fail("test not implemented yet");
+    void when_isbn_is_incorrect__then_validation_fails() {
+        var book = new Book("12", "George Orwell", "1984", 10.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The ISBN format must be valid.");
     }
 
     @Test
-    void given_author_is_missing__then_validation_fails(){
-        fail("test not implemented yet");
+    void when_author_is_missing__then_validation_fails() {
+        var book = new Book("1234567890", "George Orwell", "", 10.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book author must be defined.");
     }
 
     @Test
-    void given_author_is_incorrect__then_validation_fails(){
-        fail("test not implemented yet");
-    }
-    @Test
-    void given_price_is_missing__then_validation_fails(){
-        fail("test not implemented yet");
+    void when_title_is_missing__then_validation_fails() {
+        var book = new Book("1234567890", "", "1984", 10.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book title must be defined.");
     }
 
     @Test
-    void given_price_is_negative__then_validation_fails(){
-        fail("test not implemented yet");
+    void when_price_is_missing__then_validation_fails() {
+        var book = new Book("1234567890", "George Orwell", "1984", null);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book price must be defined.");
     }
-    
+
     @Test
-    void given_price_is_zero__then_validation_fails(){
-        fail("test not implemented yet");
+    void when_price_is_negative__then_validation_fails() {
+        var book = new Book("1234567890", "George Orwell", "1984", 0.00);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book price must be greater than zero.");
+    }
+
+    @Test
+    void when_price_is_zero__then_validation_fails() {
+        var book = new Book("1234567890", "George Orwell", "1984", -10.00);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        List<String> constraintViolationMessages = violations.stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        assertThat(constraintViolationMessages)
+                .contains("The book price must be greater than zero.");
     }
 }
