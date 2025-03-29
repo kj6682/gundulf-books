d2638c2530295e02fab126047b1a785aaaf8aef2
KJ6682 <luigi.mastroserio@gmail.com>
2025-03-29 07:45:23 +0100

    [conf] starting over
    Added postgres dependency through compose

---
+++ gundulf-catalog/src/main/java/org/kj6682/gundulfcatalog/GundulfCatalogApplication.java
@@ -0,0 +1,13 @@
+package org.kj6682.gundulfcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundulfCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundulfCatalogApplication.class, args);
+	}
+
+}
---
+++ gundulf-catalog/src/test/java/org/kj6682/gundulfcatalog/GundulfCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kj6682.gundulfcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GundulfCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
